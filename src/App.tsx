cb60eb0e2af05ef42af042fd981e2d0ac5fa5488
WalterJin2 <walterjin2@gmail.com>
2023-02-18 05:52:30 +1100

    Add Laypout, Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          
+
+          <Route path='*' element={<p>Not found!</p>} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
