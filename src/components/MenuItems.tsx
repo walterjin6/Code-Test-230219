3190ed9e0753a69869d4ec8c3e7555bf22f2d583
WalterJin2 <walterjin2@gmail.com>
2023-02-18 07:05:41 +1100

    Add menuItems component

---
+++ src/components/MenuItems.tsx
@@ -0,0 +1,108 @@
+import { useState, useEffect, useRef } from 'react'
+
+
+import { Link } from 'react-router-dom'
+type PropsType = {
+  items: any,
+  depthLevel: number
+}
+const MenuItems = ({ items, depthLevel }: PropsType) => {
+  const [dropdown, setDropdown] = useState(false)
+
+  const ref = useRef<null | HTMLLIElement>(null)
+
+  useEffect(() => {
+    const handler = (event: MouseEvent |  TouchEvent) => {
+      if (
+        dropdown &&
+        ref.current &&
+        !ref.current.contains(event.target as Node)
+      ) {
+        setDropdown(false)
+      }
+    }
+    document.addEventListener('mousedown', handler)
+    document.addEventListener('touchstart', handler)
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener('mousedown', handler)
+      document.removeEventListener('touchstart', handler)
+    }
+  }, [dropdown])
+
+  const onMouseEnter = () => {
+    window.innerWidth > 960 && setDropdown(true)
+  }
+
+  const onMouseLeave = () => {
+    window.innerWidth > 960 && setDropdown(false)
+  }
+
+  const closeDropdown = () => {
+    dropdown && setDropdown(false)
+  }
+
+  return (
+    <li
+      className='relative text-sm text-left'
+      ref={ref}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={closeDropdown}
+    >
+      {items.url && items.submenu ? (
+        <>
+          <button
+            className='flex items-center w-full text-left hover:bg-sky-200'
+            type='button'
+            aria-haspopup='menu'
+            aria-expanded={dropdown ? 'true' : 'false'}
+            onClick={() => setDropdown((prev) => !prev)}
+          >
+            {window.innerWidth < 960 && depthLevel === 0 ? (
+              items.title
+            ) : (
+              <Link
+                className='block px-4 py-3 text-left hover:bg-sky-200'
+                to={items.url}
+              >
+                {items.title}
+              </Link>
+            )}
+
+            {depthLevel > 0 && window.innerWidth < 960 ? null : depthLevel >
+                0 && window.innerWidth > 960 ? (
+              <span>&raquo;</span>
+            ) : (
+              <span className='arrow' />
+            )}
+          </button>
+
+        </>
+      ) : !items.url && items.submenu ? (
+        <>
+          <button
+            className='w-full text-left px-4 py-3 hover:bg-sky-200'
+            type='button'
+            aria-haspopup='menu'
+            aria-expanded={dropdown ? 'true' : 'false'}
+            onClick={() => setDropdown((prev) => !prev)}
+          >
+            {items.title}{' '}
+            {depthLevel > 0 ? <span>&raquo;</span> : <span className='arrow' />}
+          </button>
+   
+        </>
+      ) : (
+        <Link
+          className='block  text-left  px-4 py-3 hover:bg-sky-200'
+          to={items.url}
+        >
+          {items.title}
+        </Link>
+      )}
+    </li>
+  )
+}
+
+export default MenuItems
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,17 @@
+import { menuItems } from '../config/menuItems'
+import MenuItems from './MenuItems'
 const Navbar = () => {
-  return <nav>Navbar</nav>
+  return (
+    <nav>
+      <ul className='flex items-center flex-wrap '>
+        {menuItems.map((menu, index) => {
+          const depthLevel = 0
+          return <MenuItems items={menu} key={index} depthLevel={depthLevel} />
+        })}
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
+
