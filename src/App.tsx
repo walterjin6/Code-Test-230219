ca176833dfa437361c70035eadf9dacc65b1256d
WalterJin2 <walterjin2@gmail.com>
2023-02-18 07:52:22 +1100

    Add products component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import Products from './pages/Products'
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-
+          <Route path='products' element={<Products />} />
           <Route path='*' element={<p>Not found!</p>} />
         </Route>
       </Routes>
---
+++ src/pages/Products.tsx
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <h2>Products page</h2>
+}
+
+export default Products
